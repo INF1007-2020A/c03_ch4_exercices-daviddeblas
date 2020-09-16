89e175f32cb8e5e0327b7d387ce641c476f6ab0a
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2020-09-16 14:25:15 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,23 +3,32 @@
 
 
 def is_even_len(string: str) -> bool:
-    return False
+    return len(string)%2==0
 
 
 def remove_third_char(string: str) -> str:
-    return ""
+    return string[:2] + string[3:]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+    old_char_index = string.find(old_char)
+    return string[:old_char_index] + new_char + string[old_char_index + 1:]
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
+    c=0
+    for char in string:
+        if char == "l":
+            c = c + 1
+    return c
 
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    for character in sentence:
+        character = "Baby shark doo doo doo doo doo doo"
+        list_mot = character.split()
+        nombre_de_mot = len(list_mot)
+        return nombre_de_mot
 
 
 def main() -> None:
@@ -31,10 +40,11 @@ def main() -> None:
     print(f"On supprime le 3e caratère dans la chaine '{string}'. Résultat: {remove_third_char(string)}")
 
     string = "hello world!"
-    print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
+    print(
+        f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
 
     print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
-    
+
     string = "Baby shark doo doo doo doo doo doo"
     print(f"Le nombre de mots dans la chaine {string} est: {get_nb_words(string)}")
 
